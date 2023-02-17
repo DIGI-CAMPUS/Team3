92e9cbbca2864deb482e1a805742991291ceb0a6
unknown <geniejin96@gmail.com>
2023-02-17 17:26:58 +0900

    0217

---
+++ leejin/0217/fruits.js
@@ -0,0 +1,33 @@
+const fruits = ["apple", "orange"];
+
+// console.log(fruits);
+
+// for (let i = 0; i < fruits.length; i++) {
+// 	console.log(fruits[i]);
+// }
+
+// for (let fruit of fruits) {
+// 	console.log(fruit);
+// }
+
+// fruits.forEach(function (fruit, index) {
+// 	console.log(fruit, index);
+// });
+
+// fruits.forEach((fruit, ind
+
+//push : add an item to the end
+fruits.push("strawberry", "peach");
+console.log(fruits);
+
+//pop : remove an item from the end
+
+// fruits.pop();
+// console.log(fruits);
+
+//unshift : add un item to the begining
+fruits.unshift("remon", "banana");
+console.log(fruits);
+
+//shift :remove an item from th beniggin
+fruits.shift();
